56068ea2d49c44f23c66f650ef652cc8d7df6aca
SURIYA <ss.suriyastark@gmail.com>
2022-12-19 20:22:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import './App.css'
 import Jokes from './component/Jokes'
+import JokesData from './component/JokesData'
 function App() {
+  const jokesElements = JokesData.map(joke => {
+    return <Jokes setup={joke.setup} punchline={joke.punchline} />
+  })
   return (
     <div>
-      <Jokes
-        // setup="I got my daughter a fridge for her birthday"
-        punchline="Ican't wait to see her face light up when she open it"
-        isPun={true}
-        upvotes={10}
-        downVotes={2}
-        comments={[{author:"",body:"",title:""}]} />
-      <Jokes
-        setup="How did the hacker escape the police?"
-        punchline="He jsut ransomware"/>
-      <Jokes
-        setup="Why don't pirates travel in mountain roads?"
-        punchline="Scurvy"/>
-      <Jokes
-        setup="Why do bees stay on the hive in winter?"
-        punchline="Swarm"/>
-      <Jokes
-        setup="What's the best thing about switzerland?"
-        punchline="I dont , but the flag is big plus"/>
+      {jokesElements}
     </div>
   )
 }
---
+++ src/component/JokesData.jsx
@@ -0,0 +1,23 @@
+export default [
+    {
+        setup:"I got my daughter a fridge for her birthday",
+        punchline:"Ican't wait to see her face light up when she open it"
+    },
+    {
+        setup:"How did the hacker escape the police?",
+        punchline:"He jsut ransomware"
+    },
+    {
+        setup:"Why don't pirates travel in mountain roads?",
+        punchline:"Scurvy"
+    },
+    {
+        setup:"Why do bees stay on the hive in winter?",
+        punchline:"Swarm"
+    },
+    {
+        setup:"What's the best thing about switzerland?",
+        punchline:"I dont , but the flag is big plus"
+    }
+
+]
